8a8a90f65ca25f1cc9f9feb63fb24f2789c7d421
Demi Lee <demilee.to@gmail.com>
2017-09-19 11:55:09 -0400

    exercise2 stretch

--- exercise2.rb
+++ exercise2.rb
@@ -3,6 +3,7 @@ drama = "Inception"
 comedy = "21 Jump St"
 dramedy = "Up"
 
+
 puts "Welcome to the Movie Advisor!"
 puts "To start, rate the following genres from 1 to 5."
 puts "Documentaries?"
@@ -13,7 +14,15 @@ puts "Comedies?"
 com = gets.chomp.to_i
 
 if doc >= 4
-  puts "We recommend #{documentary}."
+  if dra >=4 && com < 3
+    puts "We recommend #{documentary} and #{drama}."
+  elsif dra < 3 && com >= 3
+    puts "We recommend #{documentary} and #{comedy}."
+  elsif dra >= 4 && com >= 4
+    puts "We recommend #{documentary} and #{dramedy}."
+  elsif dra < 3 && com < 3
+    puts "We recommend #{documentary}."
+  end
 elsif dra >= 4 && com >= 4
   puts "We recommend #{dramedy}."
 elsif dra >= 4 && com < 4
