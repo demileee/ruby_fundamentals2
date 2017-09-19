6c817ff007388d5ac49e43ff42bf3d67f2050e5f
Demi Lee <demilee.to@gmail.com>
2017-09-19 11:47:06 -0400

    exercise2

---
+++ exercise2.rb
@@ -0,0 +1,25 @@
+documentary = "Earth"
+drama = "Inception"
+comedy = "21 Jump St"
+dramedy = "Up"
+
+puts "Welcome to the Movie Advisor!"
+puts "To start, rate the following genres from 1 to 5."
+puts "Documentaries?"
+doc = gets.chomp.to_i
+puts "Dramas?"
+dra = gets.chomp.to_i
+puts "Comedies?"
+com = gets.chomp.to_i
+
+if doc >= 4
+  puts "We recommend #{documentary}."
+elsif dra >= 4 && com >= 4
+  puts "We recommend #{dramedy}."
+elsif dra >= 4 && com < 4
+  puts "We recommend #{drama}."
+elsif dra < 3 && com >= 4
+  puts "We recommend #{comedy}."
+else
+  puts "Maybe you should read a book instead, like Britannica."
+end
